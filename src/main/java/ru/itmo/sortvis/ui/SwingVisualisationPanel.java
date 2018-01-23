1dd1fbdbf81de7d7bb77d9c2dd5a215b4203abdc
Ilia Gromov [Win64] <igromovbox@gmail.com>
2018-01-23 22:47:36 +0300

    Improve look

--- src/main/java/ru/itmo/sortvis/ui/SwingVisualisationPanel.java
+++ src/main/java/ru/itmo/sortvis/ui/SwingVisualisationPanel.java
@@ -20,9 +20,20 @@ public class SwingVisualisationPanel extends JPanel implements GraphModelListene
 
 
     @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public void paintComponent(Graphics g1) {
+        super.paintComponent(g1);
+        
+        // В Graphics2D больше фич
+        Graphics2D g = (Graphics2D) g1;
+        
         g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        
+        g.setColor(Color.LIGHT_GRAY);
+        
+        // N можно оставить, а i (и возможно x0? Не очень понял, что он делает)
+        // надо убрать и переделать while циклы в for циклы. ... 
+        
         int i = 0, N = 20;
         int x0 = getSize().width / 2, y0 = getSize().height / 2;
 
@@ -31,29 +42,28 @@ public class SwingVisualisationPanel extends JPanel implements GraphModelListene
         while (i < getSize().height) {
             g.drawLine(0, i, getSize().width, i);
             i += N;
-
         }
-
+        
+        // ... Иначе где-нибудь забудете i обнулить и будете долго ловить баги.
         i = 0;
 
         while (i < getSize().width) {
             g.drawLine(i, 0, i, getSize().height);
             i += N;
-
         }
 
         // Граф
 
-        g.setColor(Color.RED);
+        g.setColor(Color.BLACK);
         while (x0 % N != 0) {
             x0 += 1;
-
         }
 
         while (y0 % N != 0) {
             y0 += 1;
-
         }
+        
+        g.setStroke(new BasicStroke(3));
 
         for (i = 0; i < coord.length / 2; i++) {
             for (int j = 0; j < coord.length / 2; j++) {
